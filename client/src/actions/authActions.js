ec4a15eb27833180e86ed756803985b62e353109
Apoorv Tiwari <apoorv.tiwari@oriserve.com>
2022-03-21 12:23:14 +0530

    locally tested.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -55,6 +55,5 @@ export const logoutUser = () => (dispatch) => {
   localStorage.removeItem("jwtToken");
   setAuthToken(false);
   dispatch(setCurrentUser({}));
-  window.location.href = "/login";
   toast.success("You have successfully logged out.")
 };
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/ping', function (req, res) {
@@ -13,9 +12,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
 app.listen(port);

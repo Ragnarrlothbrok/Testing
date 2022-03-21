34e5d3fd279f628ae15361a08384844475cc7f8b
Apoorv Tiwari <apoorv.tiwari@oriserve.com>
2022-03-21 13:16:24 +0530

    Deploy testing

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-var sslRedirect = require('heroku-ssl-redirect').default;
+// var sslRedirect = require('heroku-ssl-redirect').default;
 var express = require('express');
 const favicon = require('express-favicon');
-const path = require('path');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 const port = process.env.PORT || 8080;
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // enable ssl redirect
-app.use(sslRedirect([
-  'other',
-  'development',
-  'production'
-  ]));
+// app.use(sslRedirect([
+//   'other',
+//   'development',
+//   'production'
+//   ]));
+app.use(cors());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'client/build')));
